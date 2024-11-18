52cfadec2d026fafa75e49d65d69701dce4a2b9f
hatiaryaman <hatiaryaman@gmail.com>
2024-11-18 00:39:05 -0600

    Update main.js

--- Main-Page/main.js
+++ Main-Page/main.js
@@ -1,1 +1,13 @@
+// sign up and login
+var button = document.getElementById("signup")
+var button2 = document.getElementById("login")
+
+button.onclick = () => {
+    window.location.href = '../Sign-Up/signup.html' ;
+}
+
+button2.onclick = () => {
+    window.location.href = '../Login/login.html' ;
+}
+
 
